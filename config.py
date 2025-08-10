98b37b0d31d887d21a4cff2960a77343d07cbc06
shadowheroku <shadowheroku@gmail.com>
2025-08-10 23:13:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-BOT_TOKEN = "7847667702:AAHpPHVbkwKNN07kHZ_SoW5qTP_FtYqNp78"
+BOT_TOKEN = ""
 ADMIN_IDS = 7775049190
 CHANNEL_USERNAME, SUPPORT_LINK = "FOS_BOTS", "https://t.me/+_uWRTGAA8pJiNGZl"
 MONGODB_URI = "mongodb+srv://ryumasgod:ryumasgod@cluster0.ojfkovp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "yoruichi_bot_db"
+
 SUPPORT_CHAT = "https://t.me/+W9Qb1Iti7FQ3MDc1"
